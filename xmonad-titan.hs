c1a1fec99ff2145293752e5bf345c6b09337516a
jgrisham4 <jgrisham4@gmail.com>
2017-12-02 10:42:24 -0600

    Adding titan xmonad/xmobar configs.

---
+++ xmonad-titan.hs
@@ -0,0 +1,32 @@
+import           System.IO
+import           XMonad
+import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.ManageDocks
+import           XMonad.Layout.Spacing
+import           XMonad.Util.EZConfig     (additionalKeys)
+import           XMonad.Util.Run          (spawnPipe)
+
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+
+myBar = "xmobar"
+
+myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" , ppTitle = const ""}
+
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+myConfig = defaultConfig
+    { manageHook = manageDocks <+> manageHook defaultConfig
+    , layoutHook = avoidStruts $ myLayout
+    , modMask = mod4Mask
+    , workspaces = myWorkspaces
+    , terminal = "termite"
+    }
+
+myWorkspaces = ["1:Web", "2:Dev", "3:Docs", "4:Music", "5", "6", "7", "8"]
+
+myLayout = tiled ||| Mirror tiled ||| Full
+  where
+    tiled = smartSpacing 10 $ Tall nmaster delta ratio
+    nmaster = 1
+    ratio = 1/2
+    delta = 3/100
